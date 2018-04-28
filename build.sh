0d80bf5377f4eef046c4f3aa5049ac47e4b5027d
Ye Jia <jiayephy@gmail.com>
2018-04-27 20:29:00 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ TARGET=tab_menu.zip
 zip $TARGET           \
     manifest.json     \
     popup.*           \
+    options.*         \
     *.png             \
     jquery.min.js     \
     jquery-ui.min.js  \
+    background.js     \
     LICENSE.txt       \
     README.md
